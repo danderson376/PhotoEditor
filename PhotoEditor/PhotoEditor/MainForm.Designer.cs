96876f0a0821386e241653dd34714a4a4e3962ee
danderson376 <danderson3@harding.edu>
2019-09-20 17:00:09 -0500

    Added Tree and list view to mainform

--- PhotoEditor/PhotoEditor/MainForm.Designer.cs
+++ PhotoEditor/PhotoEditor/MainForm.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 35);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(175, 403);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(193, 35);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(595, 402);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
             this.Name = "MainForm";
             this.Text = "PhotoEditor";
             this.ResumeLayout(false);
@@ -42,6 +62,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
