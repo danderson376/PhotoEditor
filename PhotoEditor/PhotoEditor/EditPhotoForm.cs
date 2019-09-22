72cebfac962877e296f84f5ca0fc5bd103683030
danderson376 <danderson3@harding.edu>
2019-09-22 02:06:15 -0500

    Made simple list of images, made invert and tint async

--- PhotoEditor/PhotoEditor/EditPhotoForm.cs
+++ PhotoEditor/PhotoEditor/EditPhotoForm.cs
@@ -15,13 +15,14 @@ namespace PhotoEditor
         private Image myImage;
         Bitmap transformedBitmap;
 
-        public EditPhotoForm()
+        public EditPhotoForm(Image img)
         {
             InitializeComponent();
+            myImage = img;
         }
         private void EditPhotoForm_Load(object sender, EventArgs e)
         {
-            myImage = Image.FromFile("C:\\Users\\dalac\\OneDrive\\Pictures\\spidermanlogo.jpg");
+           // myImage = Image.FromFile("C:\\Users\\dalac\\OneDrive\\Pictures\\spidermanlogo.jpg");
             transformedBitmap = new Bitmap(myImage);
             LoadImage(myImage);
         }
@@ -35,7 +36,7 @@ namespace PhotoEditor
         private void SaveButton_Click(object sender, EventArgs e)
         {
             myImage = (Image)transformedBitmap;
-           // myImage.Save("myphoto.jpg", ImageFormat.Jpeg); 
+            //myImage.Save("myphoto.jpg", ImageFormat.Jpeg); 
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
@@ -43,70 +44,101 @@ namespace PhotoEditor
             //add code to reverse all changes
             Close();
         }
-        private void ColorButton_Click(object sender, EventArgs e)
+        private async void ColorButton_Click(object sender, EventArgs e)
         {
             ColorDialog colorBox = new ColorDialog();
             if (colorBox.ShowDialog() == DialogResult.OK)
             { 
-               ChangeColors(colorBox.Color);
+               await ChangeColors(colorBox.Color);
             }
             Image tintedImage = (Image)transformedBitmap;
             LoadImage(tintedImage);
         }
-        private void InvertButton_Click(object sender, EventArgs e) //need to make async, progress bar, 
+        private async void InvertButton_Click(object sender, EventArgs e) //need to make async, progress bar, 
         {
-            InvertColors();
+            await InvertColors();
             Image invertedImage = (Image)transformedBitmap;
             LoadImage(invertedImage);
         }
 
-        private void InvertColors()
+        private async Task InvertColors()
         {
-            for (int y = 0; y < transformedBitmap.Height; y++)
+            UseWaitCursor = true;
+
+            //cancellationTokenSource = new CancellationTokenSource();
+            //var token = cancellationTokenSource.Token;
+
+            await Task.Run(() =>
             {
-                for (int x = 0; x < transformedBitmap.Width; x++)
+                for (int y = 0; y < transformedBitmap.Height; y++)
                 {
-                    Color color = transformedBitmap.GetPixel(x, y);
-                    int newRed = Math.Abs(color.R - 255);
-                    int newGreen = Math.Abs(color.G - 255);
-                    int newBlue = Math.Abs(color.B - 255);
-                    Color newColor = Color.FromArgb(newRed, newGreen, newBlue);
-                    transformedBitmap.SetPixel(x, y, newColor);
+                    for (int x = 0; x < transformedBitmap.Width; x++)
+                    {
+                        try
+                        {
+                            Color color = transformedBitmap.GetPixel(x, y);
+                            int newRed = Math.Abs(color.R - 255);
+                            int newGreen = Math.Abs(color.G - 255);
+                            int newBlue = Math.Abs(color.B - 255);
+                            Color newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                            transformedBitmap.SetPixel(x, y, newColor);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Could not Invert Colors");
+                        }
+                    }
                 }
-            }
+            });
+            UseWaitCursor = false;
         }
 
-        private void ChangeColors(Color tintColor)
+        private async Task ChangeColors(Color tintColor)
         {
-            for (int y = 0; y < transformedBitmap.Height; y++)
-            {
-                for (int x = 0; x < transformedBitmap.Width; x++)
+            UseWaitCursor = true;
+
+            await Task.Run(() =>
+            { 
+                for (int y = 0; y < transformedBitmap.Height; y++)
                 {
-                    Color color = transformedBitmap.GetPixel(x, y);
-
-                    int newRed = Math.Abs((color.R + (1 - color.R/255) * tintColor.R)); //found this formula at https://stackoverflow.com/questions/4699762/how-do-i-recolor-an-image-see-images                                                                       
-                    int newGreen = Math.Abs((color.G + (1 - color.G/255) * tintColor.G)); //Author Usernames: CodesInChaos and Aliostad 
-                    int newBlue = Math.Abs((color.B + (1 - color.B/255) * tintColor.B));
-
-                    if(newRed > 255)
+                    for (int x = 0; x < transformedBitmap.Width; x++)
                     {
-                        newRed = 255;
-                    }
+                        try
+                        {
+                            Color color = transformedBitmap.GetPixel(x, y);
 
-                    if(newGreen > 255)
-                    {
-                        newGreen = 255;
-                    }
+                            int newRed = Math.Abs((color.R + (1 - color.R / 255) * tintColor.R)); //found this formula at https://stackoverflow.com/questions/4699762/how-do-i-recolor-an-image-see-images                                                                       
+                            int newGreen = Math.Abs((color.G + (1 - color.G / 255) * tintColor.G)); //Author Usernames: CodesInChaos and Aliostad 
+                            int newBlue = Math.Abs((color.B + (1 - color.B / 255) * tintColor.B));
 
-                    if(newBlue > 255)
-                    {
-                        newBlue = 255;
-                    }
+                            if (newRed > 255)
+                            {
+                                newRed = 255;
+                            }
 
-                    Color newColor = Color.FromArgb(newRed, newGreen, newBlue);
-                    transformedBitmap.SetPixel(x, y, newColor);
+                            if (newGreen > 255)
+                            {
+                                newGreen = 255;
+                            }
+
+                            if (newBlue > 255)
+                            {
+                                newBlue = 255;
+                            }
+
+                            Color newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                            transformedBitmap.SetPixel(x, y, newColor);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Could not Tint Image");
+                        }
+                    }
                 }
-            }
+            });
+
+
+            UseWaitCursor = false;
         }
 
     }
--- PhotoEditor/PhotoEditor/MainForm.cs
+++ PhotoEditor/PhotoEditor/MainForm.cs
@@ -13,6 +13,7 @@ namespace PhotoEditor
 {
     public partial class MainForm : Form
     {
+        private ListView myList;
         public MainForm()
         {
             InitializeComponent();
@@ -26,15 +27,17 @@ namespace PhotoEditor
 
         private async Task InitializePhotoList_Async() //example to get double click to work. Need to change to get photos from disk
         {                                              //feel free to change any of the code that relates to the main form, but leave EditPhotoForm to me
-            ListView lv = this.PhotoList;
-            lv.View = View.List;
+            myList = this.PhotoList;
+            ImageList images = new ImageList();
+            
+           // images.View = View.SmallIcon;
 
 
-            lv.Items.Add("John Lennon");
+            //lv.Items.Add("John Lennon");
 
             await Task.Run(() =>
             {
-                DirectoryInfo homeDir = new DirectoryInfo("C:\\Pictures");
+                DirectoryInfo homeDir = new DirectoryInfo("C:\\Users\\dalac\\OneDrive\\Pictures");
                 foreach (FileInfo file in homeDir.GetFiles("*.jpg"))
                 {
                     try
@@ -42,6 +45,12 @@ namespace PhotoEditor
                         byte[] bytes = System.IO.File.ReadAllBytes(file.FullName);
                         MemoryStream ms = new MemoryStream(bytes);
                         Image img = Image.FromStream(ms); // Use this instead of Image.FromFile()
+
+                        images.Images.Add(file.Name, img);
+                        myList.LargeImageList = images;
+                        var listViewItem = myList.Items.Add(file.Name);
+                        listViewItem.ImageKey = file.Name;
+
                         Console.WriteLine("Filename: " + file.Name);
                         Console.WriteLine("Last mod: " + file.LastWriteTime.ToString());
                         Console.WriteLine("File size: " + file.Length);
@@ -59,11 +68,12 @@ namespace PhotoEditor
         {                                                                        //Username of Author: XIVSolutions
             ListViewHitTestInfo info = PhotoList.HitTest(e.X, e.Y);
             ListViewItem item = info.Item;
+            Image image = Image.FromFile("C:\\Users\\dalac\\OneDrive\\Pictures\\" + item.ImageKey);
 
             if (item != null)
             {
                 //MessageBox.Show("The selected Item Name is: " + item.Text);
-                EditPhotoForm editBox = new EditPhotoForm();  //creates new editBox Every time an item is double clicked
+                EditPhotoForm editBox = new EditPhotoForm(image);  //creates new editBox Every time an item is double clicked
                 editBox.ShowDialog(this);
             }
             else
